43213e111d34e5d3a9659028783052538dad5b38
caco123 <willian.galvez91@gmail.com>
2023-04-24 13:38:49 +0200

    cambiando nombre de método

--- src/app/pages/users/user-detail/user-detail.component.ts
+++ src/app/pages/users/user-detail/user-detail.component.ts
@@ -1,20 +1,24 @@
-import { FormGroup, FormBuilder } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
-import { UserInterface } from 'src/app/interfaces/user.interface';
 import { User } from 'src/app/models/user.model';
 import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.scss']
+  styleUrls: ['./user-detail.component.scss'],
 })
 export class UserDetailComponent implements OnInit {
-  form!: FormGroup
-  user!: User
-  constructor(private route: ActivatedRoute, private userService: UsersService, private formBuilder: FormBuilder) { }
+  form!: FormGroup;
+  user!: User;
+
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UsersService,
+    private formBuilder: FormBuilder
+  ) {}
 
   async ngOnInit() {
     const id: number = this.route.snapshot.params['id'];
@@ -28,11 +32,11 @@ export class UserDetailComponent implements OnInit {
       username: [this.user.username],
       password: [this.user.password],
       phone: [this.user.phone],
-    })
+    });
   }
 
-  async enviar() {
+  async save() {
     const res = await lastValueFrom(this.userService.save(this.user));
-    console.log(res)
+    console.log(res);
   }
 }
